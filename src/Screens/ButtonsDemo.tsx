a0bb88b975856416b405ec289e7409f86165c36b
samiksha <samiksha_mohan.jagtap@neosofttech.com>
2020-10-01 16:32:07 +0530

    add custom button

--- src/Screens/ButtonsDemo.tsx
+++ src/Screens/ButtonsDemo.tsx
@@ -11,8 +11,9 @@ const ButtonsDemo = () => (
     <View style={styles.container}>
         <Button title='Large Button' onPress={() => onPress('This is Large Button')}/>
         <Button title='Medium Size Border Button' btnSize='medium' onPress={() => onPress('This is Medium Size Border Button')}/>
-        <Button title='Small Size Shadow Button' btnSize='small' shadowButton={true} onPress={() => onPress('This is Large Button')}/>
+        <Button title='Small Size Shadow Button' btnSize='small' shadowButton={true} onPress={() => onPress('This is Small Size Shadow Button')}/>
         <Button title='Large Size Disabled Button' disabled={true} onPress={() => {}}/>
+        <Button title='Any Cutom Button' shadowButton={true} borderStyle={styles.borderStyle} btnStyle={styles.customBtn} textStyle={styles.textStyle}  onPress={() => onPress('This Is User Styles Button')}/>
     </View>
 )
 
@@ -23,5 +24,18 @@ const styles = StyleSheet.create({
         flex: 1,
         marginHorizontal: colors.white,
         justifyContent: 'space-between'
+    },
+    borderStyle: {
+        borderWidth: 1,
+        borderColor: colors.blue50,
+        borderStyle: 'solid',
+    },
+    customBtn: {
+        width: '75%',
+        backgroundColor: colors.white
+    },
+    textStyle: {
+        color: colors.blue50,
+        fontSize: 16
     }
 })
