92c70cedddcb000f12883257d6a00206cc72fb01
Mahipal Singh <mahipal.singh-1@syngenta.com>
2020-11-23 15:47:40 +0530

    Scrollview at home

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, ScrollView } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { AlertType } from '../Components/Alert/AlertComponent'
 import AlertView from '../Components/Alert/AlertView'
@@ -29,48 +29,52 @@ const HomeScreen = ({ navigation }: any) => {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('ColorDemo')}>
-        <Text>Color System</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('CardDemo')}>
-        <Text>Card Component</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('AvatarDemo')}>
-        <Text>Avatar Component</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('LoaderDemo')}>
-        <Text>Loader Component</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('PhoneFieldWithCountryPickerDemo')}>
-        <Text>Country Picker Component</Text>
-      </TouchableOpacity>
+      <View style={{ width: '100%', height: '90%' }}>
+        <ScrollView style={{ flex: 1, bottom: 20 }} contentContainerStyle={{ alignItems: 'center' }}>
+          <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('ColorDemo')}>
+            <Text>Color System</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('CardDemo')}>
+            <Text>Card Component</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('AvatarDemo')}>
+            <Text>Avatar Component</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('LoaderDemo')}>
+            <Text>Loader Component</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('PhoneFieldWithCountryPickerDemo')}>
+            <Text>Country Picker Component</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('ButtonsDemo')}>
-        <Text>Button Component</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('ListViewDemo')}>
-        <Text>List View Component</Text>
-      </TouchableOpacity>
-      {/**Show Simple Alert */}
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() =>
-          AlertView.show(
-            'Simple alert Message',
-            AlertType.NONE,
-            'Thanks',
-            'Error Title optional',
-            'Success Title Optional'
-          )
-        }>
-        <Text>Show Simple Alert</Text>
-      </TouchableOpacity>
-      {/**Show Network connection */}
-      {netState && (
-        <Text style={{ backgroundColor: netState.isConnected ? 'green' : 'red' }}>
-          Network Type {netState.type} Connection {netState.isConnected.toString()}
-        </Text>
-      )}
+          <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('ButtonsDemo')}>
+            <Text>Button Component</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('ListViewDemo')}>
+            <Text>List View Component</Text>
+          </TouchableOpacity>
+          {/**Show Simple Alert */}
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() =>
+              AlertView.show(
+                'Simple alert Message',
+                AlertType.NONE,
+                'Thanks',
+                'Error Title optional',
+                'Success Title Optional'
+              )
+            }>
+            <Text>Show Simple Alert</Text>
+          </TouchableOpacity>
+          {/**Show Network connection */}
+          {netState && (
+            <Text style={{ backgroundColor: netState.isConnected ? 'green' : 'red' }}>
+              Network Type {netState.type} Connection {netState.isConnected.toString()}
+            </Text>
+          )}
+        </ScrollView>
+      </View>
     </View>
   )
 }
